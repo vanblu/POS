c00cc6ee55ad447cfefac1c573b94a3d25731d84
Henry TH Hung <hung1999@sas.upenn.edu>
2021-05-04 14:18:19 +0800

    More Code on Coordinates Manipulation

--- src/Coordinates.java
+++ src/Coordinates.java
@@ -10,6 +10,14 @@ public class Coordinates {
     }
     
     public static void main(String[] args) {
+        
+        System.out.println(latLongToPoint(40.017675845638266, -105.28327300072598));
+        System.out.println(latLongToPoint(40.0157246662949, -105.277114431823));
+        
+        System.out.println(latLongToPoint(40.017675845638266, -105.28327300072598).
+                distanceTo(latLongToPoint(40.0157246662949, -105.277114431823)));
+        
+        /*
         System.out.println(latLongToPoint(22.298668762693847, 114.17831561226001));
         System.out.println(latLongToPoint(22.30042008952247, 114.1747184747348));
         System.out.println(latLongToPoint(22.298668762693847, 114.17831561226001).
@@ -17,5 +25,10 @@ public class Coordinates {
         System.out.println(latLongToPoint(22.300905227518083, 114.17206010617662));
         System.out.println(latLongToPoint(22.298668762693847, 114.17831561226001).
                 distanceTo(latLongToPoint(22.300905227518083, 114.17206010617662)));
+        System.out.println(latLongToPoint(22.307690825252617, 114.19272638351435));
+        System.out.println(latLongToPoint(22.298668762693847, 114.17831561226001).
+                distanceTo(latLongToPoint(22.307690825252617, 114.19272638351435)));
+                */
+       
     }
 }
--- src/QuadTree.java
+++ src/QuadTree.java
@@ -1,5 +1,6 @@
 import java.util.Collection;
 import java.util.List;
+import java.util.TreeSet;
 
 public class QuadTree implements IQuadTree {
 
@@ -13,8 +14,29 @@ public class QuadTree implements IQuadTree {
     private QuadTree sw;
     private Collection<IRestaurant> restaurants;
     
+    // only load restaurants that are at most 8km away from user
+    final static double MAX_HALFLENGTH = 5656; 
+    // if two restaurants are less than 500m away from each other, put them in the same block
+    final static double MIN_HALFLENGTH = 353.5;
+    // this gives us maxDepth = 4
+    
+    /**
+     * initialize a QuadTree given a user's location 
+     * 
+     * @param userCoordinates Coordinates that indicate the user's location,
+     *        will be the center point of the QuadTree 
+     * 
+     * @return a QuadTree centered at userCoordinates, with "radius" of the largest block being 8km
+     */
+    
     public QuadTree(Point userCoordinates) {
-        
+        this.botRight = new Point(userCoordinates.getX() + MAX_HALFLENGTH, 
+                userCoordinates.getY() + MAX_HALFLENGTH);
+        this.topLeft = new Point(userCoordinates.getX() - MAX_HALFLENGTH, 
+                userCoordinates.getY() - MAX_HALFLENGTH);
+        this.depth = 0; 
+        this.restaurants = new TreeSet<IRestaurant>();
+
     }
     
     private void split(QuadTree head) {
@@ -23,6 +45,11 @@ public class QuadTree implements IQuadTree {
     
     private QuadTree chooseLeaf(QuadTree head, Point p) {
         return null;
+    }
+    
+    private void addRestaurantToNode(QuadTree node, IRestaurant rest) {
+        node.restaurants.add(rest);
+        return;
     }
     
     // insert a Restaurant in the quadtree based on its location info
@@ -43,6 +70,75 @@ public class QuadTree implements IQuadTree {
     @Override
     public boolean isLeaf() {
         return (ne == null);
+    }
+    
+    /************* Getters and Setters **********************/
+    
+    /**
+     * @return top left point
+     */
+    public Point getTopLeft() {
+        return this.topLeft;
+    }
+    
+    // setter for topLeft
+    public void setTopLeft(Point pt) {
+        this.topLeft = pt;
+    }
+
+    /**
+     * @return Bottom right point
+     */
+    public Point getBotRight() {
+        return this.botRight;
+    }
+
+    // setter for botRight
+    public void setBotRight(Point pt) {
+        this.botRight = pt;
+    }
+    
+
+    public QuadTree getNWTree() {
+        return this.nw;
+    }
+
+    
+    public QuadTree getNETree() {
+        return this.ne;
+    }
+
+
+    public QuadTree getSWTree() {
+        return this.sw;
+    }
+
+    public QuadTree getSETree() {
+        return this.se;
+    }
+    
+    public void setParent(QuadTree tree) {
+        this.parent = tree;
+    }
+    
+    public QuadTree getParent() {
+        return this.parent;
+    }
+    
+    public void setNETree(QuadTree tree) {
+        this.ne = tree;
+    }
+    
+    public void setNWTree(QuadTree tree) {
+        this.nw = tree;
+    }
+    
+    public void setSETree(QuadTree tree) {
+        this.se = tree;
+    }
+    
+    public void setSWTree(QuadTree tree) {
+        this.sw = tree;
     }
     
     
