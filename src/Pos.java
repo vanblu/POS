56cb41dc02d63899b3df48084dd409c79a715fa8
Henry TH Hung <hung1999@sas.upenn.edu>
2021-05-09 20:03:32 +0800

    pushing again

--- src/Pos.java
+++ src/Pos.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
@@ -7,29 +6,29 @@ import java.util.List;
 
 
 public class Pos implements IPos {
-
+    
     List<Restaurant> restaurantSet; // load restaurant to this collection
-    QuadTree head; // head of the QuadTree storing restaurants
+    QuadTree head; // head of the QuadTree storing restaurants 
     Point userCoordinates;
-
+    
 
     public int loadRestaurantInfo(String filepath) {
-        restaurantSet =  new ArrayList<>();
+
         JsonParser parse = new JsonParser();
         List<Restaurant> output = parse.read(filepath);
-        restaurantSet = output;
-
+        restaurantSet = output; 
+        
         return restaurantSet.size();
     }
 
     public QuadTree storeRestaurantsInTree(String city) {
-
+        
         // create a quadTree centered at user's location
-        head = new QuadTree(Coordinates.latLongToPoint(userCoordinates.getY(), userCoordinates.getX()));
+        head = new QuadTree(Coordinates.latLongToPoint(userCoordinates.getY(), userCoordinates.getX())); 
         for (IRestaurant r : restaurantSet) {
             if (r.getAddress().getCity().equals(city.toLowerCase())) {
                 head.insert(r);
-//            }
+            }
         }
         return head;
     }
@@ -39,7 +38,7 @@ public class Pos implements IPos {
         return head.rangeSearch(maxDist, lowRatng, highRating, cuisineType);
     }
 
-    public List<IRestaurant> sortRestaurants(List<IRestaurant> restaurants,
+    public List<IRestaurant> sortRestaurants(List<IRestaurant> restaurants, 
             String sortCriteria, boolean ascending) {
         Comparator<IRestaurant> comp = null;
         switch (sortCriteria) {
@@ -65,6 +64,7 @@ public class Pos implements IPos {
         return ret;
     }
 
+    
     public Point getUserCoordinates() {
         return this.userCoordinates;
     }
